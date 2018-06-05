49b012e99bdd1832342267b3e417637af7bd190d
Julio César <jcesarmobile@gmail.com>
2018-06-05 20:11:03 +0200

    Redirect launchURL to http://localhost:8080

--- src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
+++ src/android/com/ionicframework/cordova/webview/IonicWebViewEngine.java
@@ -1,11 +1,13 @@
 package com.ionicframework.cordova.webview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 
+import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPreferences;
 import org.apache.cordova.CordovaResourceApi;
@@ -43,23 +45,38 @@ public class IonicWebViewEngine extends SystemWebViewEngine {
   public void init(CordovaWebView parentWebView, CordovaInterface cordova, final CordovaWebViewEngine.Client client,
                    CordovaResourceApi resourceApi, PluginManager pluginManager,
                    NativeToJsMessageQueue nativeToJsMessageQueue) {
-    localServer = new WebViewLocalServer(cordova.getActivity(), "localhost:8080", true);
+    ConfigXmlParser parser = new ConfigXmlParser();
+    parser.parse(cordova.getContext());
+
+    localServer = new WebViewLocalServer(cordova.getActivity(), "localhost:8080", true, parser);
     WebViewLocalServer.AssetHostingDetails ahd = localServer.hostAssets("www");
 
-    webView.setWebViewClient(new ServerClient(this));
+    webView.setWebViewClient(new ServerClient(this, parser));
 
     super.init(parentWebView, cordova, client, resourceApi, pluginManager, nativeToJsMessageQueue);
   }
 
   private class ServerClient extends SystemWebViewClient
   {
-    public ServerClient(SystemWebViewEngine parentEngine) {
+    private ConfigXmlParser parser;
+
+    public ServerClient(SystemWebViewEngine parentEngine, ConfigXmlParser parser) {
       super(parentEngine);
+      this.parser = parser;
     }
 
     @Override
     public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
       return localServer.shouldInterceptRequest(request);
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+      super.onPageStarted(view, url, favicon);
+      if (url.equals(parser.getLaunchUrl())) {
+        view.stopLoading();
+        view.loadUrl("http://localhost:8080/");
+      }
     }
   }
 
