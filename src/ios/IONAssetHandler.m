c7868599b991be482c091e081a35dbc8f18115c4
Tobias Herrmann <toby.herrmann@gmail.com>
2019-10-30 09:22:33 +0100

    fix(ionassethandler.m): fix startPath is getting null
    If Ionic is used inside a framework file, self.basePath is null and stringToLoad contains the full
    needed path. Current string concatenation does not respect that case, leading to startPath is null
    and so index.html is not loaded. Fix this by setting startPath to empty string instead of null if
    base path is null.

--- src/ios/IONAssetHandler.m
+++ src/ios/IONAssetHandler.m
@@ -28,7 +28,7 @@
         if ([stringToLoad hasPrefix:@"/_app_file_"]) {
             startPath = [stringToLoad stringByReplacingOccurrencesOfString:@"/_app_file_" withString:@""];
         } else {
-            startPath = self.basePath;
+            startPath = self.basePath ? self.basePath : @"";
             if ([stringToLoad isEqualToString:@""] || [url.pathExtension isEqualToString:@""]) {
                 startPath = [startPath stringByAppendingString:@"/index.html"];
             } else {
