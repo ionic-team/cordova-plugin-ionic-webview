1b3d15cebbfe92b23f455b28eb295a56ddb24086
Niklas Merz <nmerz@gedys-intraware.de>
2020-03-04 11:44:28 +0100

    Move boolean check

--- src/www/util.js
+++ src/www/util.js
@@ -13,10 +13,10 @@ var WebView = {
     if (url.startsWith('file://')) {
       return window.WEBVIEW_SERVER_URL + url.replace('file://', '/_app_file_');
     }
-    if (url.startsWith('http://') && convertHttp) {
+    if (convertHttp && url.startsWith('http://')) {
       return window.WEBVIEW_SERVER_URL + url.replace('http://', '/_http_proxy_');
     }
-    if (url.startsWith('https://') && convertHttp) {
+    if (convertHttp && url.startsWith('https://')) {
       return window.WEBVIEW_SERVER_URL + url.replace('https://', '/_https_proxy_');
     }
     if (url.startsWith('content://')) {
